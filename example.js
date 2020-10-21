a5b9ab73a8da138f5cff3bcf8f3f265cd85010bb
Nathan Snyder <nathan@snydern.com>
2020-10-20 22:28:45 -0400

    Handle more errors

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 import ArcherC7 from './lib/ArcherC7.js';
 
 const example = async function() {
-  const archer = new ArcherC7('http://192.168.1.1', 'email', 'password');
+  const archer = new ArcherC7('http://192.168.1.1', 'email', 'password', { debug: true });
   
   const devices = await archer.fetchDevices();
   console.log(devices);
