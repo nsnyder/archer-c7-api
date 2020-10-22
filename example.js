da5a36baf646095879e16ae6e1191e95a7f710b5
Nathan Snyder <nathan@snydern.com>
2020-10-21 23:15:08 -0400

    Implement logging out of the router

--- example.js
+++ example.js
@@ -1,10 +1,10 @@
 import ArcherC7 from './lib/ArcherC7.js';
 
-const example = async function() {
+(async function() {
   const archer = new ArcherC7('http://192.168.1.1', 'email', 'password', { debug: true });
   
   const devices = await archer.fetchDevices();
   console.log(devices);
-}
 
-example();
+  await archer.logout();
+})();
